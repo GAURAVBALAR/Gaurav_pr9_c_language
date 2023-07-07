2761d1611186f64dbb9d88ac7f1b72e2471cd417
GAURAVBALAR <133326741+GAURAVBALAR@users.noreply.github.com>
2023-07-07 13:08:11 +0530

    Add files via upload
    First

---
+++ pr91.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+	FILE *E;
+	FILE *F;
+	int even[100];
+	int odd[100];
+	int i;
+	E = fopen("even.txt","w");
+	F = fopen("odd.txt","w");
+	if(E == NULL){
+		printf("File is Unable to Open....");
+	}else{
+		printf("EVEN File opened...\n");
+	}
+	if(F == NULL){
+		printf("File is Unable to Open....");
+	}else{
+		printf("ODD File opened...");
+	}
+	for(i=50;i<=70;i++){
+			if(i%2==0){
+		fprintf(E,"%d\n",i);
+	}
+    else{
+    	fprintf(F,"%d\n",i);
+	}
+        
+		}
+	
+	return 0;
+}
